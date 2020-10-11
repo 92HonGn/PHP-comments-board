5c23a17cbb09a0ef30660ed1c5c6fb76ad4747b3
謝政宏 <b3194359@gmail.com>
2020-10-11 23:53:11 +0800

    註冊功能

--- check_register.php
+++ check_register.php
@@ -1,4 +1,5 @@
 <?php
+  session_start();
   require_once('connect/connent_DB.php');
 
   $username = $_POST['username'];
@@ -7,7 +8,7 @@
 
   $check_sql = "SELECT username, nickname FROM $user_table"."WHERE username=:username OR nickname=:nickname";
 
-  $check_sth = $conn->prepare($sql);
+  $check_sth = $conn->prepare($check_sql);
 
   $check_sth->bindParam(':username', $username);
   $check_sth->bindParam(':nickname', $nickname);
@@ -17,28 +18,34 @@
   $check_sth->setFetchMode(PDO::FETCH_ASSOC);
 
   if ($check_sth->rowCount() === 0 ){
-    
-  } else {
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-  }
+    $register_sql = "INSERT INTO $user_table (username, password, nickname"."VALUES (:username, :password, :nickname)";
 
+    $register_sth = $conn->prepare($register_sql);
 
+    $param = [
+      ':username' => $username,
+      ':password' => $hashed_password,
+      ':nickname' => $nickname,
+    ];
 
-  $sqlSelect = "SELECT * FROM $users_table where username='" . $username . "'" ;
-
-  if ($username && $password ) {
-    $sqlInsert = "INSERT INTO $users_table(username,password) VALUES('$username','$password')";
-    $result = $conn->query($sqlInsert);
-    if ($result) {
-       echo 'Register success';
-    } else {
-       echo 'Register failed';
+    if ($register_sth->execute($param)){
+      $_SESSION['user_id'] = $conn->lastInsertId();
+      echo 'ok';
     }
+
   } else {
-    echo '請輸入正確的格式' ;
-?>
-    <br>
-    <a href="index.php">Back to register</a>
-<?php
+    while($check_row = $check_sth->fetch()){
+      if(!strcasecmp($check_row['username'],$username) AND !strcasecmp($check_row['nickname'],$nickname)){
+        echo '帳號匿名錯誤';
+      }else if(!strcasecmp($check_row['nickname'],$nickname)){
+        echo '匿名錯誤';
+      }else if(!strcasecmp($check_ro['username'],$username)){
+        echo '帳號錯誤';
+      }else{
+        echo '例外錯誤';
+      }
+    }
   }
 ?>
